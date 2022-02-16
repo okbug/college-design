1cd0b7620954af93b7100910bb2e2063fe7e79e4
okbug <1583978087@qq.com>
2022-02-16 23:41:13 +0800

    feat: 翻转编辑器组件

--- docute/src/App.tsx
+++ docute/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Editor from "./components/Editor";
+
 function App() {
   return (
     <div className="App">
       <h1>React 子应用</h1>
-      <div id="editor"></div>
+      <Editor />
     </div>
   );
 }
--- docute/webpack.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  devServer: {
-    contentBase: "./build",
-    port: 8081, // 端口号
-    inline: true,
-    hot: true,
-    historyApiFallback: true,
-  },
-};
