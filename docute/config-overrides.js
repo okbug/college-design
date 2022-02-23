f9dbc94b137260d49a6768b14fdeab6b64a196f7
okbug <1583978087@qq.com>
2022-02-23 11:11:50 +0800

    feat: 完成对别名的设置

--- docute/config-overrides.js
+++ docute/config-overrides.js
@@ -1,24 +1,26 @@
 const {
-    override,
-    overrideDevServer
-  } = require('customize-cra')
-  const path = require('path')
-  const packageName = require('./package.json').name
+  override,
+  overrideDevServer
+} = require("customize-cra");
 
-  module.exports = {
-      webpack: override((config) => {
-        config.output = config.output || {}
-        config.output.library = `${packageName}-[name]`
-        config.output.libraryTarget = 'umd'
-        config.output.chunkLoadingGlobal = `webpackJsonp_${packageName}` // jsonpFunction https://github.com/webpack/webpack.js.org/issues/3940
-        return config
-      }),
-      'devServer': overrideDevServer(
-        (config) => {
-          config.headers = config.headers || {}
-          config.headers['Access-Control-Allow-Origin'] = '*';
-          config.historyApiFallback = true;
-          return config
-        }
-      )
-  }
+const path = require("path");
+const packageName = require("./package.json").name;
+
+module.exports = {
+  webpack: override((config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.output = config.output || {};
+    config.output.library = `${packageName}-[name]`;
+    config.output.libraryTarget = "umd";
+    config.output.chunkLoadingGlobal = `webpackJsonp_${packageName}`; // jsonpFunction https://github.com/webpack/webpack.js.org/issues/3940
+    return config;
+  }),
+  devServer: overrideDevServer((config) => {
+    config.headers = config.headers || {};
+    config.headers["Access-Control-Allow-Origin"] = "*";
+    config.historyApiFallback = true;
+    return config;
+  })
+};
