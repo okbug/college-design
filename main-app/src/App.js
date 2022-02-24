c40147527c0fac1567416df70dad5968a3d610ce
okbug <1583978087@qq.com>
2022-02-24 13:10:20 +0800

    feat: 设置后端

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -5,7 +5,5 @@ import PlayGround from './Layout/PlayGround.jsx'
 
 export default () => (<>
     <Header />
-    <Link to="/doc" className='toDoc'>在线文档</Link>
-    <Link to="/ppt" className='toPPT'>在线演示文稿</Link>
     <PlayGround />
 </>)
